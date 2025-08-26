fdc76cad652af6527cdf19baf41105da8b441fd4
Victor Pizzuto <victor.pizzuto@hotmail.com>
2025-08-26 20:53:51 -0300

    feat: created timeseries application

---
+++ apps/sensorhub_elixir_timeseries/lib/sensorhub_elixir_timeseries.ex
@@ -0,0 +1,18 @@
+defmodule SensorhubElixirTimeseries do
+  @moduledoc """
+  Documentation for `SensorhubElixirTimeseries`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SensorhubElixirTimeseries.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/sensorhub_elixir_timeseries/test/sensorhub_elixir_timeseries_test.exs
@@ -0,0 +1,8 @@
+defmodule SensorhubElixirTimeseriesTest do
+  use ExUnit.Case
+  doctest SensorhubElixirTimeseries
+
+  test "greets the world" do
+    assert SensorhubElixirTimeseries.hello() == :world
+  end
+end
