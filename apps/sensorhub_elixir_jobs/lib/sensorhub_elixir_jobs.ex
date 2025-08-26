8804a6e8f24fa01c462607b19975ddc9650f566e
Victor Pizzuto <victor.pizzuto@hotmail.com>
2025-08-26 20:57:11 -0300

    feat: created jobs application

---
+++ apps/sensorhub_elixir_jobs/lib/sensorhub_elixir_jobs.ex
@@ -0,0 +1,18 @@
+defmodule SensorhubElixirJobs do
+  @moduledoc """
+  Documentation for `SensorhubElixirJobs`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SensorhubElixirJobs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/sensorhub_elixir_jobs/test/sensorhub_elixir_jobs_test.exs
@@ -0,0 +1,8 @@
+defmodule SensorhubElixirJobsTest do
+  use ExUnit.Case
+  doctest SensorhubElixirJobs
+
+  test "greets the world" do
+    assert SensorhubElixirJobs.hello() == :world
+  end
+end
