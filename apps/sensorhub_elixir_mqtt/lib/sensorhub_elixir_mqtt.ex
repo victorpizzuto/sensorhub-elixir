3ac3b196bd5eb517c7913ff659575c912970b861
Victor Pizzuto <victor.pizzuto@hotmail.com>
2025-08-26 20:58:04 -0300

    feat: created mqtt application

---
+++ apps/sensorhub_elixir_mqtt/lib/sensorhub_elixir_mqtt.ex
@@ -0,0 +1,18 @@
+defmodule SensorhubElixirMqtt do
+  @moduledoc """
+  Documentation for `SensorhubElixirMqtt`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SensorhubElixirMqtt.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/sensorhub_elixir_mqtt/test/sensorhub_elixir_mqtt_test.exs
@@ -0,0 +1,8 @@
+defmodule SensorhubElixirMqttTest do
+  use ExUnit.Case
+  doctest SensorhubElixirMqtt
+
+  test "greets the world" do
+    assert SensorhubElixirMqtt.hello() == :world
+  end
+end
